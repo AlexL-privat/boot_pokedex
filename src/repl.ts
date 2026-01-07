c1d9c5edbccb9f7630913ef2312801b02ef0db76
AlexL-privat <alexander.lauenroth@gmail.com>
2026-01-07 12:23:24 +0100

    Added REPL

---
+++ src/repl.ts
@@ -0,0 +1,26 @@
+import { createInterface } from "readline";
+
+export function cleanInput(input: string): string[] {
+    return input.toLowerCase().split(" ").filter((s) => s.trim().length > 0);
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "pokedex> ",
+    });
+
+    rl.prompt();
+    
+    rl.on("line", (line) => {
+        const commands = cleanInput(line);
+        if (commands.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${commands[0]}`);
+        // Here you would handle other commands as needed
+        rl.prompt();
+    })
+}
